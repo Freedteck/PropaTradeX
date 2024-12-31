243a425a43008c3b17afe0d95300b82df2fef608
Freedteck <freedteck@gmail.com>
2024-12-31 04:25:35 +0100

    Update: Update Hero component

--- src/routes/explore/Explore.jsx
+++ src/routes/explore/Explore.jsx
@@ -6,7 +6,7 @@ import styles from "./Explore.module.css";
 const Explore = () => {
   return (
     <div className={styles.explore}>
-      <Hero />
+      <Hero isExplore={true} />
       <div className={styles.all}>
         <PropertyList
           showViewAll={false}
