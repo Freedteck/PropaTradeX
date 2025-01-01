f54b8f8a58c1682e1bce46ace91ee78755617c9f
Freedteck <freedteck@gmail.com>
2025-01-01 07:56:50 +0100

    Add icon to Buitton component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-const Button = ({ btnClass, label }) => {
-  return <button className={styles[btnClass]}>{label}</button>;
+const Button = ({ btnClass, label, icon }) => {
+  return (
+    <button className={styles[btnClass]}>
+      {icon && <span className={styles.icon}>{icon}</span>}
+      {label}
+    </button>
+  );
 };
 
 Button.propTypes = {
   btnClass: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  icon: PropTypes.element,
 };
 
 export default Button;
