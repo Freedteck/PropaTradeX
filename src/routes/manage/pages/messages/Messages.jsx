75b5ac35a8242ef1d0d9bff2abfd5f27bad0104a
Olalekan Opeyemi <147733443+olamilekan5162@users.noreply.github.com>
2025-01-03 20:21:16 +0000

    work on message

--- src/routes/manage/pages/messages/Messages.jsx
+++ src/routes/manage/pages/messages/Messages.jsx
@@ -1,14 +1,24 @@
 import UserMessageCard from "../../../../components/userMessageCard/UserMessageCard";
-import OtherMessageCard from "../../../../components/messageCardFrom/OtherMessageCard";
+import OtherMessageCard from "../../../../components/otherMessageCard/OtherMessageCard";
 import styles from "./Messages.module.css"
 
 const Messages = () => {
   return (
+    <>
     <div className={styles.messages}>
       <h1>Messages</h1>
       <UserMessageCard text="I cherish you" timeStamp="2:25 PM"/>
       <OtherMessageCard text="I love you" timeStamp="2:25 PM"/>
     </div>
+    <form className={styles.form}>
+      <input 
+      type="text"
+      placeholder="Enter your message"/>
+      <button type="submit">
+        Send
+      </button>
+    </form>
+    </>
   );
 };
 
