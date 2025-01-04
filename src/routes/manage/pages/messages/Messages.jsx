d857b89444db0c58f3b40cd43a346bbb5ae04b14
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-01-04 19:24:02 +0100

    Add form for sending messages

--- src/routes/manage/pages/messages/Messages.jsx
+++ src/routes/manage/pages/messages/Messages.jsx
@@ -4,21 +4,31 @@ import styles from "./Messages.module.css"
 
 const Messages = () => {
   return (
-    <>
     <div className={styles.messages}>
       <h1>Messages</h1>
-      <UserMessageCard text="I cherish you" timeStamp="2:25 PM"/>
-      <OtherMessageCard text="I love you" timeStamp="2:25 PM"/>
+      <div className={styles.chatContainer}>
+        <UserMessageCard 
+        text="Hello, I Would like to make enquiries on your listed house at Lekki, Lagos."
+        timeStamp="2:25 PM"/>
+        
+        <OtherMessageCard 
+        text="Hi, good to meet you. The house is a well furnished 3 bedroom apartment" 
+        timeStamp="2:25 PM"/>
+
+        <UserMessageCard 
+        text="How much does it cost"
+        timeStamp="2:25 PM"/>
+      </div>
+
+      <form className={styles.form}>
+        <input 
+        type="text"
+        placeholder="Enter your message"/>
+        <button type="submit">
+          Send
+        </button>
+      </form>
     </div>
-    <form className={styles.form}>
-      <input 
-      type="text"
-      placeholder="Enter your message"/>
-      <button type="submit">
-        Send
-      </button>
-    </form>
-    </>
   );
 };
 
