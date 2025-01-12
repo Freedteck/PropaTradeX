cafb4dc585dcd8ee14914100014a48197f3c40af
Freedteck <freedteck@gmail.com>
2025-01-12 20:10:27 +0100

    Fix issue with pinata config

--- src/utils/pinataConfig.js
+++ src/utils/pinataConfig.js
@@ -1,9 +1,9 @@
-// import { PinataSDK } from "pinata-web3";
+import { PinataSDK } from "pinata-web3";
 
-// export const pinata = new PinataSDK({
-//   pinataJwt: `${import.meta.env.VITE_PINATA_JWT}`,
-//   pinataGateway: `${import.meta.env.VITE_GATEWAY_URL}`,
-// });
+export const pinata = new PinataSDK({
+  pinataJwt: `${import.meta.env.VITE_PINATA_JWT}`,
+  pinataGateway: `${import.meta.env.VITE_GATEWAY_URL}`,
+});
 
 // export const pinataMessage = new PinataSDK({
 //   pinataJwt: `${import.meta.env.VITE_PINATA_MESSAGE_JWT}`,
