29ceb99bab410e52a6ad590d9fedac047e803b90
Freedteck <freedteck@gmail.com>
2025-01-12 19:56:49 +0100

    Update Pinata config

--- src/utils/pinataConfig.js
+++ src/utils/pinataConfig.js
@@ -1,12 +1,11 @@
-import { PinataSDK } from "pinata-web3";
+// import { PinataSDK } from "pinata-web3";
 
-export const pinata = new PinataSDK({
-  pinataJwt: `${import.meta.env.VITE_PINATA_JWT}`,
-  pinataGateway: `${import.meta.env.VITE_GATEWAY_URL}`,
-});
+// export const pinata = new PinataSDK({
+//   pinataJwt: `${import.meta.env.VITE_PINATA_JWT}`,
+//   pinataGateway: `${import.meta.env.VITE_GATEWAY_URL}`,
+// });
 
-export const pinataMessage = new PinataSDK({
-  pinataJwt: `${import.meta.env.VITE_PINATA_MESSAGE_JWT}`,
-  pinataGateway: `${import.meta.env.VITE_MESSAGE_GATEWAY_URL}`,
-});
-
+// export const pinataMessage = new PinataSDK({
+//   pinataJwt: `${import.meta.env.VITE_PINATA_MESSAGE_JWT}`,
+//   pinataGateway: `${import.meta.env.VITE_MESSAGE_GATEWAY_URL}`,
+// });
