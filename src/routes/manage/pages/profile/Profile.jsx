211205e7815a3b5754aeefe2643de9906ff026bd
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-01-12 10:58:57 +0100

    Add hero to profile

--- src/routes/manage/pages/profile/Profile.jsx
+++ src/routes/manage/pages/profile/Profile.jsx
@@ -4,64 +4,21 @@ import { useNavigate } from "react-router-dom";
 import { useAccount} from "wagmi";
 import { Avatar } from "connectkit";
 import Button from "../../../../components/button/Button";
+import Hero from "../../../../components/hero/Hero";
 
 const Profile = () => {
     const navigate = useNavigate()
     const { address } = useAccount()
-    const [userData, setUserData] = useState({
-        name: "",
-        address: "",
-        followers: 10,
-        totalContent: 6,
-        rentals: 4,
-        subscriptionStatus: false,
-
-    });
       
   return (
-    <div className={styles.profilePage}>
-      <div className={styles.profileHeader}>
-        <Avatar address={address} size={80} />
-        <p className={styles.address}>{address}</p>
+    <>
+      <Hero header="My Profile"/>
+      <div className={styles.profile}>
+        <h1>{address || "Please Connect Wallet"}</h1>
+      {/*<Button label="Edit Profile" btnClass="primary" handleClick={() => navigate("/manage/profile/edit")} />*/}
       </div>
-      <Button label="Edit Profile" btnClass="primary" handleClick={() => navigate("/manage/profile/edit")} />
-
-      <div className={styles.profileStats}>
-        <h3>Subscription</h3>
-        <p
-          className={
-            userData.subscriptionStatus ? styles.subscribed : styles.notSubscribed
-          }
-        >
-          {userData.subscriptionStatus ? "Subscribed" : "Not Subscribed"}
-        </p>
-        <div className={styles.statsGrid}>
-          <div>
-            <h4>{userData.followers}</h4>
-            <p>Followers</p>
-          </div>
-          <div>
-            <h4>{userData.totalContent}</h4>
-            <p>Total Content</p>
-          </div>
-          <div>
-            <h4>{userData.rentals}</h4>
-            <p>Rentals</p>
-          </div>
-          <div>
-            <h4>{userData.subscriptionStatus ? "Available" : "Not Available"}</h4>
-            <p>Content with Subscription</p>
-          </div>
-        </div>
-      </div>
-    
-      {!userData.subscriptionStatus && (
-        <Button btnClass="secondary" label="Subscribe"
-          handleClick={() => alert("Subscribe functionality coming soon!")}
-        />
-      )}
-      
-    </div>
+ 
+    </>
   );
 };
 
